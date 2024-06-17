fd003c10cf5843fc00c51e9750ac87b7ac5640ce
Cjay Landero <xjaylandero@gmail.com>
2024-06-17 22:53:31 +0800

    added calculation of winner

--- src/Board.tsx
+++ src/Board.tsx
@@ -2,6 +2,25 @@
 import { useState } from "react"
 import Square from "./Square"
 
+function calculateWinner(squares : string[]) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 
 export default function Board() {
 const [player, setPlayer] = useState('x')
@@ -9,13 +28,19 @@ const [squares, setSquares] = useState(Array(9).fill(null))
 
 
 function handleClick(idx : number) {
-    const nextSquares = squares.slice();
 
-    nextSquares[idx] === null ? nextSquares[idx] = player === "x" ? "X" : "O" : nextSquares[idx] 
-    setPlayer(player === "x" ? "o" : "x");
-    setSquares(nextSquares);
-  }
+  if(calculateWinner(squares)) return
+  const nextSquares = squares.slice();
+  nextSquares[idx] === null ? nextSquares[idx] = player === "x" ? "X" : "O" : nextSquares[idx] 
+  setPlayer(player === "x" ? "o" : "x");
+  setSquares(nextSquares);
+}
+
+let status = calculateWinner(squares) !== null ? `Winner: ${calculateWinner(squares)}` : `Player: ${player}`
+
   return (
+    <>
+    {status}
     <div className="grid grid-cols-3 grid-rows-3">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
         <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
@@ -27,5 +52,6 @@ function handleClick(idx : number) {
         <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
         <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
     </div>
+    </>
   )
 }
